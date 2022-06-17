c2e8ed2b6c526218b0cef3d4f3a0ede9e57c404a
Princechrix2 <princechrix2@gmail.com>
2022-06-17 18:57:30 +0200

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Every one!</h1>
+      <p>Start editing to see some magic Sure happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-quayp7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-quayp7)
